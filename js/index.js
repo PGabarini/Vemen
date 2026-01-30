32907b7a27d9f859f4f9186781146aba4c73f139
PGabarini <pagabarini@frba.utn.edu.ar>
2026-01-29 21:12:00 -0300

    animacion cambio

--- js/index.js
+++ js/index.js
@@ -1,4 +1,31 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    const herobg = document.getElementsByClassName('hero__bg')[0]
+    if(herobg){
+        cambiarPager(herobg)
+    }else{
+        console.log("no hay")
+    }
+    
+
 });
+
+const fondos = ["imagenes/autoelectrico2.png","imagenes/autoelectrico.png"]
+let turnoFondo = 0
+
+function cambiarPager(herobg){
+    
+
+    setInterval(()=>{
+        
+
+        fondoactual =  fondos[turnoFondo]
+
+        herobg.style.background = `url(${fondoactual}) center / cover no-repeat`;
+
+        turnoFondo = (turnoFondo + 1) % fondos.length
+
+        
+    },3000)
+}
