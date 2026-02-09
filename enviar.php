78fde0005bfe1deb8648f0dc43f6075ed05f8ab9
PGabarini <pagabarini@frba.utn.edu.ar>
2026-02-08 23:20:12 -0300

    envio mail

--- enviar.php
+++ enviar.php
@@ -4,27 +4,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre   = htmlspecialchars($_POST['nombre']);
     $telefono = htmlspecialchars($_POST['telefono'] ?? 'No informado');
     $email    = htmlspecialchars($_POST['email']);
-    $plan     = htmlspecialchars($_POST['plan'] ?? 'No seleccionado');
+    $articulo = htmlspecialchars($_POST['articulo'] ?? 'No seleccionado');
     $mensaje  = htmlspecialchars($_POST['mensaje']);
 
-    $destinatario = "pablogabarini@gmail.com";
+    $destinatario = "pgabarini@vemen.com.ar";
     $asunto = "Nueva consulta desde el sitio web de VEMEN";
 
     $contenido = "
-    Nombre: $nombre
-    Email: $email
-    Teléfono: $telefono
-    Plan: $plan
+Nombre: $nombre
+Email: $email
+Teléfono: $telefono
+Artículo: $articulo
 
-    Mensaje:
-    $mensaje
-    ";
+Mensaje:
+$mensaje
+";
 
-    $headers = "From: $email\r\n";
+    $headers  = "From: VEMEN <contacto@vemen.com.ar>\r\n";
     $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     if (mail($destinatario, $asunto, $contenido, $headers)) {
-        echo "Mensaje enviado correctamente";
+        echo "Mensaje enviado correctamente a pgabarini@vemen.com.ar";
     } else {
         echo "Error al enviar el mensaje";
     }
